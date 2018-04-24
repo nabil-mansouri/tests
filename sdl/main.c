c8f53e81c39a9c7fd50c60033d5e0e959bdb7e26
Nessim Rahmoun <nerahmou@z2r5p5.le-101.fr>
2018-04-24 15:55:25 +0200

    piscine php

--- sdl/main.c
+++ sdl/main.c
@@ -6,7 +6,7 @@
 /*   By: nerahmou <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/23 16:04:05 by nerahmou     #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/23 19:50:35 by nerahmou    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/24 12:32:54 by nerahmou    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,8 @@ int main(int argc, char *argv[])
 {
 	SDL_Window	*window;
 	SDL_Renderer *renderer;
-
+	SDL_Event        events;
+	int done = 0;				
 	window = NULL;
 	renderer = NULL;
 	if (SDL_Init(SDL_INIT_VIDEO))
@@ -27,28 +28,24 @@ int main(int argc, char *argv[])
 		exit(EXIT_FAILURE);
 	}
 
-	window = SDL_CreateWindow("Nessman",
-			SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,  800, 600, 0);
-	
-	if (!window)
-	{
+	window = SDL_CreateWindow("Nessman", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
+/*	{
 		SDL_Log("ERREUR : Creation fenetre > %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
-	if (!renderer)
+	while (!done)
 	{
-		SDL_Log("ERREUR : Rendu > %s\n", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
-	SDL_RenderPresent(renderer);
-	SDL_RenderClear(renderer);
-	
-	SDL_Delay(6000);
+		SDL_WaitEvent(&events);
 
+		if (events.window.event == SDL_WINDOWEVENT_CLOSE)
+			done = 1;
+	}
+	*/
+	while (!done)
+		SDL_WaitEvent(&events);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
-	
+
 	SDL_Quit();
 	return 0;
 }
