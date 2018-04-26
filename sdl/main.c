d3200b5282e6eacdf82cf81a4818caa59a8d8f46
Nessim Rahmoun <nerahmou@z2r1p6.le-101.fr>
2018-04-26 17:30:55 +0200

    add fdf

--- sdl/main.c
+++ sdl/main.c
@@ -6,7 +6,7 @@
 /*   By: nerahmou <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/04/23 16:04:05 by nerahmou     #+#   ##    ##    #+#       */
-/*   Updated: 2018/04/24 12:32:54 by nerahmou    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/04/26 16:52:54 by nerahmou    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,20 +19,22 @@ int main(int argc, char *argv[])
 	SDL_Window	*window;
 	SDL_Renderer *renderer;
 	SDL_Event        events;
-	int done = 0;				
+	/*SDL_Rect	rectangle;
+	int done = 0;
 	window = NULL;
 	renderer = NULL;
+	rectangle.x = 20;
+	rectangle.y = 20;
+	rectangle.w = 20;
+	rectangle.x = 15;
+*/
 	if (SDL_Init(SDL_INIT_VIDEO))
-	{
-		SDL_Log("ERREUR : Initialisation SDL > %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
-	}
-
-	window = SDL_CreateWindow("Nessman", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
-/*	{
-		SDL_Log("ERREUR : Creation fenetre > %s\n", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
+	window =SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+			                              640, 480, SDL_WINDOW_ALLOW_HIGHDPI);
+	SDL_Delay(3333);
+	/*renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	SDL_RenderPresent(renderer);
 	while (!done)
 	{
 		SDL_WaitEvent(&events);
@@ -40,12 +42,9 @@ int main(int argc, char *argv[])
 		if (events.window.event == SDL_WINDOWEVENT_CLOSE)
 			done = 1;
 	}
-	*/
-	while (!done)
-		SDL_WaitEvent(&events);
+*/	
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
-
 	SDL_Quit();
 	return 0;
 }
