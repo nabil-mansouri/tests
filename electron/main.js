cd69581913bcbbc20d1f3f6634ad70f505350602
nabil-mansouri <nabil.mansouri.3@gmail.com>
2018-06-13 21:05:29 +0200

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -4,21 +4,53 @@ const BrowserWindow = electron.BrowserWindow;
 const getStdin = require('get-stdin');
 const data = require('./getters');
 
+class Application {
+
+  createWindow() {
+    this.mainWindow = new BrowserWindow({
+      width: 1800,
+      height: 1200,
+      icon: 'ant.png'
+    });
+    this.mainWindow.webContents.openDevTools();
+    this.mainWindow.loadURL(`file://${__dirname}/index.html`);
+    this.ready = new Promise((resolve, reject) => {
+      this.mainWindow.webContents.on('did-finish-load', () => {
+        resolve();
+      })
+    })
+  }
+  readStdin() {
+    this.ready.then(_ => {
+      getStdin().then(str => {
+        this.mainWindow.webContents.send('test', data.get_data(str))
+      })
+    })
+  }
+}
+Application.ready = null;
+//const app=new Application
+//app.createWindow();
+//app.readStdin();
 var mainWindow;
 
 function createWindow() {
+  console.log("SEND WINDOW")
   getStdin().then(str => {
     mainWindow = new BrowserWindow({
       width: 1800,
       height: 1200,
       icon: 'ant.png'
     });
+    mainWindow.webContents.openDevTools();
     mainWindow.loadURL(`file://${__dirname}/index.html`);
-    data.get_data(mainWindow, str.split("\n"));
+    mainWindow.webContents.on('did-finish-load', () => {
+      console.log("SEND DATA", str)
+      mainWindow.webContents.send('test', data.get_data(str))
+    })
     mainWindow.on('closed', () => {
       mainWindow = null;
     });
-    m
   });
 }
 
@@ -34,10 +66,4 @@ app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
-});
-
-let ants = 0;
-
-getStdin().then(str => {
-  data.get_data(mainWindow, str.split("\n").pop());
 });
